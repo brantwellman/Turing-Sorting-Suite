05813c528cab13b71323b8ea9eaf2ca1a19f0226
Brant Wellman <BrantWellman@gmail.com>
2015-10-18 21:24:59 -0600

    Cleaned up MergeSort. Not runnable

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,40 +1,48 @@
+require 'pry'
+
 class MergeSort
-  # split array in half down to base case (Two or One???!!!!) - slice
+  # split array in half down to base case - Arrays w/ single element
   # compare first element (first array) to second element (2nd array)
-  # Put lower element first and higher element second (push) or shovel
+  # Put lower element first and higher element second - unshift
+  # Use recursion
 
-  # comparitive -
-
-  def initialize
-    @sorted_array = []
+  def halve(array)
+    return array if array.size <= 1
+    array1 = array.slice(0, array.size/2)
+    array2 = array.slice(array.size/2, array.size)
+    halve(array1)
+    halve(array2)
   end
 
   def sort(array)
+    return array if array.size <= 0
 
-    return if array1.size < 0
+    sorted_first = sort(array1)
+    sorted_second = sort(array2)
 
-    array1 = array.slice(size/2.to_i)
-    array2 = array.slice(size/2.to_i)
-
-    # Call sort method here
-    sort(array)
-    merge(array1, array2)
-
+    merge(sorted_first, sorted_second)
   end
 
   def merge(array1, array2)
-    if array1 < array2
-      @sorted_array << array1
-    else
-      @sorted_array << array2
-    end
+    sorted_array = []
+      if array1.first > array2.first
+        sorted_array.unshift(array2.first)
+        array2.shift
+      else
+        sorted_array.unshift(array1.first)
+        array1.shift
+      end
+    sorted_array
   end
 end
 
-
-# # array = ["d", "b", "a", "c"]
+# array1 = [3]
+# array2 = [4]
+# array = [4, 3, 2, 1]
+# array = ["d", "b", "a", "c"]
 # array = [2, -1, 1, 2, 3, 4, 5, 4]
-array = [2, 1, 5, 4, -3]
+# array = [2, 1, 5, 4, -3]
 # array = (1..100).to_a.shuffle
-sorter = MergeSort.new
-p sorter.sort(array)
+# sorter = MergeSort.new
+# sorter.halve(array)
+# sorter.merge(array1, array2)
